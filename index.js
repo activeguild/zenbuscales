f92fcc8c6426c2526258666eaeb82d9c1dc5de93
j1ngzoue <workdrive0306@gmail.com>
2024-08-27 15:34:09 +0900

    chore

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const fs = require('fs');
-const path = require('path');
-const { NodeIO } = require('@gltf-transform/core');
-const { vec3 } = require('gl-matrix');
+const fs = require("fs");
+const path = require("path");
+const { NodeIO } = require("@gltf-transform/core");
+const { vec3 } = require("gl-matrix");
 
-const directoryPath = './before';
-const scaleFactor = 0.5;  // 50%縮小
+const inputPath = "./input";
+const outputPath = "./output";
+const scaleFactor = 0.1; // 50%縮小
 
 const applyGlobalScale = (root, scaleFactor) => {
-    const rootNode = root.listNodes()[0];
-    const scale = rootNode.getScale();
-    vec3.scale(scale, scale, scaleFactor);
-    rootNode.setScale(scale);
+  const rootNode = root.listNodes()[0];
+  const scale = rootNode.getScale();
+  vec3.scale(scale, scale, scaleFactor);
+  rootNode.setScale(scale);
 };
 
-const scaleModelsInDirectory = async (directory, scaleFactor) => {
-    const io = new NodeIO();
+const scaleModelsInDirectory = async (directory, outputDirectory) => {
+  const io = new NodeIO();
 
-    fs.readdirSync(directory).forEach(async file => {
-        if (path.extname(file) === '.glb') {
-            const filePath = path.join(directory, file);
-            console.log(`Processing: ${filePath}`);
+  fs.readdirSync(directory).forEach(async (file) => {
+    const filePath = path.join(directory, file);
+    const outputFullPath = path.join(outputDirectory, file);
+    const stats = fs.statSync(filePath);
 
-            const document = await io.read(filePath);
-            const root = document.getRoot();
+    if (stats.isDirectory()) {
+      // ディレクトリの場合、再帰的に処理を続ける
+      if (!fs.existsSync(outputFullPath)) {
+        fs.mkdirSync(outputFullPath, { recursive: true });
+      }
+      scaleModelsInDirectory(filePath, outputFullPath);
+    } else if (path.extname(file) === ".glb") {
+      console.log(`Processing: ${filePath}`);
 
-            applyGlobalScale(root, scaleFactor);
+      const document = await io.read(filePath);
+      const root = document.getRoot();
 
-            const outputFilePath = path.join(directory, 'scaled_' + file);
-            io.write(outputFilePath, document);
-            console.log(`Saved: ${outputFilePath}`);
-        }
-    });
+      applyGlobalScale(root, scaleFactor);
+
+      const outputFilePath = path.join(outputDirectory, file);
+      io.write(outputFilePath, document);
+      console.log(`Saved: ${outputFilePath}`);
+    }
+  });
 };
 
-scaleModelsInDirectory(directoryPath, scaleFactor);
+scaleModelsInDirectory(inputPath, outputPath);
